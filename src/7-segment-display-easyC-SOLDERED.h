2b95cba481a3cc8846ce9a3c7005cf1afb823cfe
Robert Sorić <robert.soric@gmail.com>
2024-06-18 16:25:54 +0200

    Updating library

--- src/7-segment-display-easyC-SOLDERED.h
+++ src/7-segment-display-easyC-SOLDERED.h
@@ -1,7 +1,7 @@
 /**
  **************************************************
  *
- * @file        Generic-easyC-SOLDERED.h
+ * @file        7-segment-display-easyC-SOLDERED.h
  * @brief       Header file for sensor specific code.
  *
  *
@@ -9,8 +9,8 @@
  * @authors     @ soldered.com
  ***************************************************/
 
-#ifndef __SENSOR__
-#define __SENSOR__
+#ifndef __7SEGMENT_EASYC__
+#define __7SEGMENT_EASYC__
 
 #include "Arduino.h"
 #include "libs/Generic-easyC/easyC.hpp"
@@ -21,7 +21,7 @@ class SevenSegment : public EasyC
     SevenSegment();
     void setPins(uint8_t _pins);
     void setBrightness(uint8_t _brightness);
-    void displayChar(char * _char);
+    void displayChar(char *_char);
     void displayNumber(uint8_t _num);
 
   protected:
@@ -30,7 +30,7 @@ class SevenSegment : public EasyC
   private:
     int pin;
     uint8_t currentPins = 0b11111111; // Set initial state to all off
-    uint8_t brightness = 128; // Set max brightness for initial
+    uint8_t brightness = 128;         // Set max brightness for initial
 };
 
 #endif
